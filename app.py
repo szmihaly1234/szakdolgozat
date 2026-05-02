7205a9ec2432ccf68059733d69a4faf00fbba4c9
Szűcs Mihály <66309643+szmihaly1234@users.noreply.github.com>
2026-05-02 17:45:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,49 +15,79 @@ from geopy.geocoders import ArcGIS
 # 1. ARCHITEKTÚRA (UNet)
 # ===============================
 # Ez a rész kritikus: pontosan olyannak kell lennie, mint a notebookodban!
+# ===============================
+# JAVÍTOTT ARCHITEKTÚRA (A SÚLYOK ALAPJÁN)
+# ===============================
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
         return self.conv(x)
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+    def __init__(self, in_channels=3, out_channels=1):
         super(UNet, self).__init__()
-        self.ups = nn.ModuleList()
-        self.downs = nn.ModuleList()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
         # Down part
-        for feature in features:
-            self.downs.append(DoubleConv(in_channels, feature))
-            in_channels = feature
-
+        self.down1 = DoubleConv(in_channels, 64)
+        self.down2 = DoubleConv(64, 128)
+        self.down3 = DoubleConv(128, 256)
+        self.down4 = DoubleConv(256, 512)
+        
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bottleneck = DoubleConv(512, 1024)
+        
         # Up part
-        for feature in reversed(features):
-            self.ups.append(
-                nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2)
-            )
-            self.ups.append(DoubleConv(feature*2, feature))
-
-        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
-        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+        self.up4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.conv4 = DoubleConv(1024, 512)
+        
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.conv3 = DoubleConv(512, 256)
+        
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.conv2 = DoubleConv(256, 128)
+        
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.conv1 = DoubleConv(128, 64)
+        
+        self.out = nn.Conv2d(64, out_channels, kernel_size=1)
 
     def forward(self, x):
-        skip_connections = []
-        for down in self.downs:
-            x = down(x)
-            skip_connections.append(x)
-            x = self.pool(x)
+        d1 = self.down1(x)
+        p1 = self.pool(d1)
+        
+        d2 = self.down2(p1)
+        p2 = self.pool(d2)
+        
+        d3 = self.down3(p2)
+        p3 = self.pool(d3)
+        
+        d4 = self.down4(p3)
+        p4 = self.pool(d4)
+        
+        bn = self.bottleneck(p4)
+        
+        u4 = self.up4(bn)
+        c4 = self.conv4(torch.cat([d4, u4], dim=1))
+        
+        u3 = self.up3(c4)
+        c3 = self.conv3(torch.cat([d3, u3], dim=1))
+        
+        u2 = self.up2(c3)
+        c2 = self.conv2(torch.cat([d2, u2], dim=1))
+        
+        u1 = self.up1(c2)
+        c1 = self.conv1(torch.cat([d1, u1], dim=1))
+        
+        return self.out(c1)
 
         x = self.bottleneck(x)
         skip_connections = skip_connections[::-1]
